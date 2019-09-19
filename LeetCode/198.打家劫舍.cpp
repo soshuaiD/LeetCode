958d0839a7bf6e6411f5a725bab57a1c3261960c
soshuaiD <46822127+soshuaiD@users.noreply.github.com>
2019-09-19 20:57:02 +0800

    狗日的ljq 9.19也没有做题呢

--- LeetCode/198.打家劫舍.cpp
+++ LeetCode/198.打家劫舍.cpp
@@ -1,39 +1,39 @@
-#include <iostream>
-#include <string>
-#include <cmath>
-#include <stack>
-#include <queue>
-#include <map>
-#include <vector>
-#include <cstdio>
-using namespace std;
-#pragma warning(disable:4996)
-
-int rob(vector<int>& nums) {
-	if (nums.size()==0)
-	{
-		return 0;
-	}
-
-	if (nums.size() == 1)
-		return nums[0];
-
-	int first = nums[0];
-	int second = max(nums[0], nums[1]);
-	if (nums.size() == 2)
-		return second;
-
-	for (int i = 2; i < nums.size(); i++)
-	{
-		int sum = max(first + nums[i], second);
-		first = second;
-		second = sum;
-	}
-	return second;
-	
-}
-
-int main()
-{
-	system("pause"); 
-}
+//#include <iostream>
+//#include <string>
+//#include <cmath>
+//#include <stack>
+//#include <queue>
+//#include <map>
+//#include <vector>
+//#include <cstdio>
+//using namespace std;
+//#pragma warning(disable:4996)
+//
+//int rob(vector<int>& nums) {
+//	if (nums.size()==0)
+//	{
+//		return 0;
+//	}
+//
+//	if (nums.size() == 1)
+//		return nums[0];
+//
+//	int first = nums[0];
+//	int second = max(nums[0], nums[1]);
+//	if (nums.size() == 2)
+//		return second;
+//
+//	for (int i = 2; i < nums.size(); i++)
+//	{
+//		int sum = max(first + nums[i], second);
+//		first = second;
+//		second = sum;
+//	}
+//	return second;
+//	
+//}
+//
+//int main()
+//{
+//	system("pause"); 
+//}
