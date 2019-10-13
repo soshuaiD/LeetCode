93d596d0045aa4ec18e957bb60c59eefb7840275
soshuaiD <46822127+soshuaiD@users.noreply.github.com>
2019-10-13 17:47:41 +0800

    why does someone seldom commit???

--- LeetCode/1124. 表现良好的最长时间段.cpp
+++ LeetCode/1124. 表现良好的最长时间段.cpp
@@ -12,19 +12,31 @@
 //
 //int longestWPI(vector<int>& hours) {
 //	int maxlength = 0;
-//	int length = 0;
 //	stack<int> s;
+//	vector<int> pre(hours.size() + 1, 0);
 //	for (int i = 0; i < hours.size(); i++)
 //	{
-//
+//		pre[i + 1] = pre[i] + (hours[i] > 8 ? 1 : -1);
+//		if (s.empty()||pre[s.top()]>pre[i])
+//		{
+//			s.push(i);
+//		}
 //	}
 //
+//	for (int i = hours.size(); i >maxlength; i--)
+//	{
+//		while (!s.empty()&&pre[i]>pre[s.top()])
+//		{
+//			maxlength = max(maxlength, i - s.top());
+//			s.pop();
+//		}
+//	}
 //	return maxlength;
 //}
 //
 //int main()
 //{
-//	vector<int> hours = { 9,9,6,0,6,6,9 };
+//	vector<int> hours = { 6,6,9 };
 //	cout << longestWPI(hours) << endl;
 //	system("pause"); 
 //}
