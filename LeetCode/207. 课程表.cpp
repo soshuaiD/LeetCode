73818982e55f94d6c2ba9813243f110536610c77
soshuaiD <46822127+soshuaiD@users.noreply.github.com>
2019-11-03 21:45:38 +0800

    复杂度好差，wsm???

--- LeetCode/207. 课程表.cpp
+++ LeetCode/207. 课程表.cpp
@@ -20,7 +20,8 @@ using namespace std;
 //};
 
 bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-	vector<vector<int>> graph(numCourses, vector<int>(numCourses));
+	vector<int> indegree(numCourses,0);
+	vector<vector<int>> graph(numCourses, vector<int>(numCourses, 0));
 	if (prerequisites.empty())
 	{
 		return true;
@@ -28,52 +29,55 @@ bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
 	for (auto pre:prerequisites )
 	{
 		graph[pre[1]][pre[0]] = 1;
+		indegree[pre[0]]++;
 	}
 
-	
-	stack<int> s;
-
+	queue<int> q;
+	int CourseNum = numCourses;
 	for (int i = 0; i < numCourses; i++)
 	{
-		for (int j = 0; j < numCourses; j++)
+		if (indegree[i]==0)
 		{
-			
-			vector<int> vis(numCourses, 0);
-			vis[i] = 1;
-			if (graph[i][j]==0)
+			q.push(i);
+			CourseNum--;
+		}
+	}
+	
+	while (!q.empty())
+	{
+		int pre = q.front();
+		q.pop();
+		for (int i=0;i<numCourses;i++)
+		{
+			if (graph[pre][i]==1)
+			{
+				
+				if ((--indegree[i])==0)
+				{
+					q.push(i);
+					CourseNum--;
+				}
+			}
+			else
 			{
 				continue;
 			}
-			if (graph[i][j] == 1)
-			{
-				vis[j] = 1;
-				s.push(j);
-				while (!s.empty())
-				{
-					int now = s.top();
-					s.pop();
-					for (int i1 = 0; i1 < numCourses; i1++)
-					{
-						if (graph[now][i1] == 0)
-						{
-							continue;
-						}
-						if (graph[now][i1] == 1 && vis[i1] == 0)
-						{
-							s.push(i1);
-						}
-						else if (graph[now][i1] == 1 && vis[i1] == 1)
-							return false;
-					}
-				}
-			}
 		}
 	}
-	return true;
+	if (CourseNum==0)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
 
 int main()
 {
+	vector<vector<int>> v = { {0,1} };
+	cout << canFinish(2, v) << endl;
 	system("pause"); 
 }
 
