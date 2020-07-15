ec318b5d27d3fad43c624cfcd4e0527083c01327
soshuaiD <46822127+soshuaiD@users.noreply.github.com>
2020-07-15 16:32:01 +0800

    简单计算器

--- LeetCode/main.cpp
+++ LeetCode/main.cpp
@@ -1,53 +1,53 @@
-#include <algorithm>
-#include <cassert>
-#include <deque>
-#include <functional>
-#include <iostream>
-#include <iterator>
-#include <limits>
-#include <list>
-#include <map>
-#include <math.h>
-#include <numeric>
-#include <queue>
-#include <sstream>
-#include <stack>
-#include <string>
-#include <unordered_map>
-#include <unordered_set>
-#include <utility>
-#include <vector>
-
-using namespace std;
-
-int cmp(const int& a, const int& b) {
-	return a > b;
-}
-
-void wiggleSort(vector<int>& nums) {
-	sort(nums.begin(), nums.end(), cmp);
-
-	if (nums.size() % 2)
-	{
-		for (int i = 0; i < (nums.size()+1) / 2; i++) {
-			nums.insert(nums.begin() + 2 * i, nums[nums.size() / 2 + i]);
-			nums.erase(nums.begin() + (nums.size()-1) / 2 + i+1);
-		}
-	}
-	else {
-		for (int i = 0; i < nums.size() / 2; i++) {
-			nums.insert(nums.begin() + 2 * i , nums[nums.size() / 2 + i]);
-			nums.erase(nums.begin() + (nums.size()-1) / 2 + i+1);
-		}
-	}
-}
-
-int main() {
-	vector<int> nums = {
-		1,1,2,1,2,2,1};
-	wiggleSort(nums);
-	for (int i = 0; i < nums.size(); i++)
-	{
-		cout << nums[i] << " ";
-	}
-}
+//#include <algorithm>
+//#include <cassert>
+//#include <deque>
+//#include <functional>
+//#include <iostream>
+//#include <iterator>
+//#include <limits>
+//#include <list>
+//#include <map>
+//#include <math.h>
+//#include <numeric>
+//#include <queue>
+//#include <sstream>
+//#include <stack>
+//#include <string>
+//#include <unordered_map>
+//#include <unordered_set>
+//#include <utility>
+//#include <vector>
+//
+//using namespace std;
+//
+//int cmp(const int& a, const int& b) {
+//	return a > b;
+//}
+//
+//void wiggleSort(vector<int>& nums) {
+//	sort(nums.begin(), nums.end(), cmp);
+//
+//	if (nums.size() % 2)
+//	{
+//		for (int i = 0; i < (nums.size()+1) / 2; i++) {
+//			nums.insert(nums.begin() + 2 * i, nums[nums.size() / 2 + i]);
+//			nums.erase(nums.begin() + (nums.size()-1) / 2 + i+1);
+//		}
+//	}
+//	else {
+//		for (int i = 0; i < nums.size() / 2; i++) {
+//			nums.insert(nums.begin() + 2 * i , nums[nums.size() / 2 + i]);
+//			nums.erase(nums.begin() + (nums.size()-1) / 2 + i+1);
+//		}
+//	}
+//}
+//
+//int main() {
+//	vector<int> nums = {
+//		1,1,2,1,2,2,1};
+//	wiggleSort(nums);
+//	for (int i = 0; i < nums.size(); i++)
+//	{
+//		cout << nums[i] << " ";
+//	}
+//}
---
+++ LeetCode/面试题 16.26. 计算器.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <algorithm>
+#include <stack>
+#include <string>
+#include <set>
+#include <stdlib.h>
+
+using namespace std;
+
+int calculate(string s) {
+	int ans = 0;
+
+	stack<int> opr;
+	stack<char> opt;
+
+	set<char> optSet = { '+','-','*','/',' ' };
+
+	for (int i = 0; i < s.size(); i++)
+	{
+		if (optSet.count(s[i]))
+		{
+			if (s[i] == '+' || s[i] == '-')
+			{
+				while (!opt.empty()) {
+					int num2 = opr.top();
+					opr.pop();
+					int num1 = opr.top();
+					opr.pop();
+					if (opt.top() == '*')
+					{
+						opr.push(num1 * num2);
+					}
+					else if ( opt.top() == '/') {
+						opr.push(num1 / num2);
+					}
+					else if (opt.top() == '+') {
+						opr.push(num1 + num2);
+					}
+					else if ( opt.top() == '-') {
+						opr.push(num1 - num2);
+					}
+					opt.pop();
+				}
+				opt.push(s[i]);
+			}
+			else if (s[i] == '*' || s[i] == '/')
+			{
+				if (!opt.empty()&& opt.top() == '*')
+				{
+					int num2 = opr.top();
+					opr.pop();
+					int num1 = opr.top();
+					opr.pop();
+					opr.push(num1 * num2); 
+					opt.pop();
+				}
+				else if (!opt.empty() && opt.top() == '/')
+				{
+					int num2 = opr.top();
+					opr.pop();
+					int num1 = opr.top();
+					opr.pop();
+					opr.push(num1 / num2);
+					opt.pop();
+				}
+				opt.push(s[i]);
+			}
+			else {
+				continue;
+			}
+		}
+		else {
+			int num= 0;
+			for (; i < s.size(); i++) {
+				if (s[i]<='9'&&s[i]>='0')
+				{
+					num = num * 10 - '0' + s[i];
+				}
+				else{
+					break;
+				}
+			}
+			opr.push(num);
+			i--;
+		}
+	}
+	while (opr.size()!=1)
+	{
+		int num2 = opr.top();
+		opr.pop();
+		int num1 = opr.top();
+		opr.pop();
+		switch (opt.top())
+		{
+		case '+':opr.push(num1 + num2); break;
+		case '-':opr.push(num1 - num2); break;
+		case '*':opr.push(num1 * num2); break;
+		case '/':opr.push(num1 / num2); break;
+		default:
+			cout << "Error!" << endl;
+			break;
+		}
+		opt.pop();
+	}
+	return opr.top();
+}
+
+int main() {
+	cout << calculate("1*2-3/4+5*6-7*8+9/10") << endl;
+}
