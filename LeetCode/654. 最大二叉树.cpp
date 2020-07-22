9faa85412ebfb5c3d68948f344bf7cf68d2ed490
soshuaiD <46822127+soshuaiD@users.noreply.github.com>
2020-07-22 18:43:52 +0800

    分治

--- LeetCode/654. 最大二叉树.cpp
+++ LeetCode/654. 最大二叉树.cpp
@@ -10,13 +10,56 @@
 #pragma warning(disable:4996)
 using namespace std;
 
-struct TreeNode {
-	int val;
-	TreeNode* left;
-	TreeNode* right;
-	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
-
-TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
-
-}
+//struct TreeNode {
+//	int val;
+//	TreeNode* left;
+//	TreeNode* right;
+//	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+//};
+//
+//TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
+//	if (nums.size()==0)
+//	{
+//		return nullptr;
+//	}
+//	TreeNode* root = new TreeNode(INT_MIN);
+//	int index = -1;
+//	for (int i = 0; i < nums.size(); i++)
+//	{
+//		if (nums[i]>root->val)
+//		{
+//			root->val = nums[i];
+//			index = i;
+//		}
+//	}
+//	vector<int> leftNums(nums.begin(), nums.begin() + index);
+//	root->left = constructMaximumBinaryTree(leftNums);
+//	vector<int> rightNums(nums.begin() + index + 1, nums.end());
+//	root->right = constructMaximumBinaryTree(rightNums);
+//
+//	return root;
+//
+//}
+//TreeNode* dfs(vector<int>& nums, int left, int right) {
+//	if (left > right)
+//	{
+//		return nullptr;
+//	}
+//	TreeNode* root = new TreeNode(INT_MIN);
+//	int index = -1;
+//	for (int i = left; i <= right; i++)
+//	{
+//		if (nums[i]>root->val)
+//		{
+//			root->val = nums[i];
+//			index = i;
+//		}
+//	}
+//	root->left = dfs(nums,left,index-1);
+//	root->right = dfs(nums, index + 1, right);
+//	return root;
+//}
+//
+//TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
+//	return dfs(nums, 0, nums.size() - 1);
+//}
