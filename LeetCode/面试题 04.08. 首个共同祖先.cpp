179155166a2b1e4fdba2c7a9411abd8bced510ee
soshuaiD <46822127+soshuaiD@users.noreply.github.com>
2020-08-03 20:29:08 +0800

    递归

---
+++ LeetCode/面试题 04.08. 首个共同祖先.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <stack>
+#include <queue>
+#include <map>
+#include <vector>
+#include <cstdio>
+#include <algorithm>
+#include <tuple>
+#pragma warning(disable:4996)
+using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+	if (root == nullptr || root == p || root == q)
+	{
+		return root;
+	}
+	TreeNode* left = lowestCommonAncestor(root->left, p, q);
+	TreeNode* right = lowestCommonAncestor(root->right, p, q);
+	if (left && right)
+	{
+		return root;
+	}
+	else if (left) {
+		return left;
+	}
+	else {
+		return right;
+	}
+	return nullptr;
+}
